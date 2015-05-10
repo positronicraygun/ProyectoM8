abae4cbacb123c7a4b749ea70b11cac7962e8514
david <david@david-desktop>
2015-05-10 17:58:21 +0200

    Cambio1

--- src/programam8/ProgramaM8.java
+++ src/programam8/ProgramaM8.java
@@ -16,7 +16,7 @@ public class ProgramaM8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ORIGINAL");
+        System.out.println("Cambio1");
     }
     
 }
